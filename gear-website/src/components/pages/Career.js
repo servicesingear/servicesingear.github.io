b94846130ae82f89fb045be6fdc77a55c0551901
sivaadda <sivapriyaadda@gmail.com>
2025-04-03 06:12:32 +0530

    Career page adding

---
+++ gear-website/src/components/pages/Career.js
@@ -0,0 +1,74 @@
+
+import '../styles/Career.css';
+import jobsData from '../data/jobs.json';
+import React, { useState } from 'react';
+import HorizontalJobCard from './JobCard';
+
+const Career = () => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const googleFormURL = 'https://forms.gle/JHCSjonKdXZ1HWaY8';
+    const filteredJobs = jobsData.filter((job) =>
+        job.jobTitle.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.industry.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.location.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+
+  return (
+    <div>
+    <section className="career-header-section">
+      <div className="container-fluid">
+        <div className="row">
+          {/* Left aligned content */}
+          <div className="col-md-10 text-left">
+            <p className="line-one">Home / Career</p>
+            <p className="line-two">Career</p>
+            <p className="line-three">Discover Excellence In Your Career</p>
+          </div>
+        </div>
+      </div>
+      </section>
+      <section className="career-content-section">
+      <div className="container">
+        {/* Centered Header with Animation */}
+        <div className="section-header animated fadeInDown">
+          <h2>JOIN OUR TEAM</h2>
+          <p>Find the perfect opportunity for your future</p>
+        </div>
+
+        {/* Search Bar with Animation */}
+       
+        <div className="search-bar animated fadeInUp">
+          <div className="input-group">
+            
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by job title, industry, or location..."
+              aria-label="Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <span className="input-group-text search-icon">
+              <i className="fa fa-search"></i>
+            </span>
+          </div>
+        </div>
+        <div className="container my-4">
+     
+      <div className="row">
+        {jobsData.map((job) => (
+          <div key={job.id} className='col-12 col-md-7 col-lg-5' >
+            <HorizontalJobCard job={job} googleFormURL={googleFormURL} />
+          </div>
+        ))}
+      </div>
+    </div>
+        </div>
+    </section>
+       </div>
+      
+  );
+};
+
+export default Career;
--- gear-website/src/index.js
+++ gear-website/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { BrowserRouter } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
